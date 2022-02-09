3b75330c603f3da67ba5f38a4408565052bdf09c
Foobware <92938692+Foobware@users.noreply.github.com>
2022-02-09 20:25:01 +0000

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,33 @@
+-- settings
+getgenv().SilentAimbotKey = 'b'; getgenv().AimlockKey = 'q';  _G.WEBHOOK_URL = ''; getgenv().UserKey = 'yourkeygoeshere';
+-- no touchies
+getgenv().UserKey = 'yourkeygoeshere';
+-- bypasses whitelist
+setreadonly(syn,false)
+synr=syn.request
+mr = hookfunction(math.random,function(...)
+    if checkcaller() then
+        return 1
+    end
+    return mr(...)
+end)
+syn.request = function(data)
+    if data.Url:find("loadScript") then
+        return {Body="13a0d940e9ade58b716df047bc5764aac44545bdafa4da3aaef7966853f45491ca591bb0a460e1601268c29b1143496914e00db48e88dbcb1e34f0f1b19554ff"}
+    end
+    if data.Url:find("xd") then
+        return {Body="c59028bda721a768b0f6d9a4cb65fe9cab89e88d03d74af2b3c57b2eb646d2efcb4a073afc2d685f61389df1cd3143e4f9ae7124454a7613e3320d108dec1a16"}
+    end
+    return synr(data)
+end
+t=true
+mb = hookfunction(messagebox,function(...)
+    if t then
+        t=false
+        return
+    end
+    return mb(...)
+end)
+setreadonly(syn, true)
+-- the code for ka
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Foobware/KITTENASSIST-CRAKED-LOLOLOL/main/Kittenassist.lua'))()
